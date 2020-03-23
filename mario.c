333efada53c2d1e4b0ac201e61337fdbf191144a
tlamen <tlamen@users.noreply.github.com>
2020-03-23 17:47:59 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -9,42 +9,42 @@ int main(void)
         n = get_int("Height:\n");
     } while(n < 1 || n > 8);
     //"Space counter" 1 + print blocks
-    for(int i = 0; i < n-1; i++) {
+    for(int i = 0; i < n - 1; i++) {
         printf(" ");
     }
     printf("#\n");
     //"Space counter" 2 + print blocks(using if clause)
-    for(int i = 0; i < n-2; i++) {
+    for(int i = 0; i < n - 2; i++) {
         printf(" ");
     }
     if(n > 1) printf("##\n");
     //"Space counter" 3 + print blocks(using if clause)
-     for(int i = 0; i < n-3; i++) {
+     for(int i = 0; i < n - 3; i++) {
         printf(" ");
     }
     if(n > 2) printf("###\n");
     //"Space counter" 4 + print blocks(using if clause)
-     for(int i = 0; i < n-4; i++) {
+     for(int i = 0; i < n - 4; i++) {
         printf(" ");
     }
     if(n > 3) printf("####\n");
     //"Space counter" 5 + print blocks(using if clause)
-     for(int i = 0; i < n-5; i++) {
+     for(int i = 0; i < n - 5; i++) {
         printf(" ");
     }
     if(n > 4) printf("#####\n");
     //"Space counter" 6 + print blocks(using if clause)
-     for(int i = 0; i < n-6; i++) {
+     for(int i = 0; i < n - 6; i++) {
         printf(" ");
     }
     if(n > 5) printf("######\n");
     //"Space counter" 7 + print blocks(using if clause)
-     for(int i = 0; i < n-7; i++) {
+     for(int i = 0; i < n - 7; i++) {
         printf(" ");
     }
     if(n > 6) printf("#######\n");
     //"Space counter" 8 + print blocks(using if clause)
-     for(int i = 0; i < n-8; i++) {
+     for(int i = 0; i < n - 8; i++) {
         printf(" ");
     }
     if(n > 7) printf("########\n");
