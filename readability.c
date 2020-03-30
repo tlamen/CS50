1640848a52a76871695cf25ada582e931f7cc720
tlamen <tlamen@users.noreply.github.com>
2020-03-30 19:58:05 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -7,35 +7,32 @@ int main(void) {
     //getting text
     string text = get_string("Text: \n");
     //preparing variables
-    int length = strlen(text);
-    int letters = 0;
-    int words = 1;
-    int sentences = 0;
+    double length = strlen(text);
+    double letters = 0.0;
+    double words = 1.0;
+    double sentences = 0.0;
     //Running through characters
     for(int i = 0; i < length; i++) {
         //counting number of letters
         if(text[i] > 64 & text[i] < 91) {
-            letters = letters + 1;
+            letters = letters + 1.0;
         }
         if(text[i] > 96 & text[i] < 123) {
-            letters = letters + 1;
+            letters = letters + 1.0;
         }
         //counting number of words
         if(text[i] == 32) {
-            words = words + 1;
+            words = words + 1.0;
         }
         //counting number of sentences
         if(text[i] == 33 | text[i] == 46 | text[i] == 63) {
-            sentences = sentences + 1;
+            sentences = sentences + 1.0;
         }
     }
-    if(sentences == 0) {
-        sentences = 1;
+    if(sentences == 0.0) {
+        sentences = 1.0;
     }
-    //converting int valeus into float values
-    float fletters = (float) letters;
-    float fwords = (float) words;
-    float fsentences = (float) sentences;
+
     //Indices L e S
     double ele = 100 * letters / words;
     double esse = 100 * sentences / words;
@@ -44,7 +41,7 @@ int main(void) {
 
     //output:
     if(index < 1) {
-        printf("Below grade 1.\n");
+        printf("Before grade 1.\n");
     }
     if(index >= 1 & index <= 16) {
         printf("Grade %i\n", (int) round(index));
