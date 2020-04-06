9b23cf190d6988195e2d3d5d71a83854cf5c20da
Tlamen <Tlamen@users.noreply.github.com>
2020-04-06 16:18:33 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -10,7 +10,7 @@ int main(int argc, string argv[]) {
 
     if (argc != 2) {
         printf("Usage: ./substitution KEY\n");
-        return 0;
+        return 1;
     }
 
     //check if key is valid 1
